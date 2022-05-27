f3f26e0458010ecb9fd94d1a387848af2ee3a232
BTAkademimcsd <53770975+BTAkademimcsd@users.noreply.github.com>
2022-05-27 21:32:48 +0300

    Add project files.

---
+++ MvcGiris/Controllers/DenemeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcGiris.Controllers
+{
+    public class DenemeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // asp.net ve asp.net core mvc'de request controller(action) yapılır. Url üzerinden fiziki sayfa çağrılamaz......
+            return View();
+            // View metodu cshtml uzantılı özel bir dosya dönmektedir. Yani mvc'de fiziki dosya çağrılamadığı için ve reqeusti Controller action metodu karşıladğı için kullanıcıya cshtml uzantlı dosya dönemktedir. Bu cshtml uzantılı dosya aspnet core mvc'ki viewengine sayesinde üretilir...Yani bu sayfa derlenme aşamasında fiziki olarak durrsada kullanıcı için çalışma zamanında üretilir...
+
+            // csharphtml dosyasıdır..
+            // cshtml dosyalarına html'e ek olarak c# komutlarıda yazılır... 
+        }
+
+        public IActionResult Sayfam()
+        {
+            return View();
+        }
+
+        public IActionResult Deneme()
+        {
+            return View("demo"); // demo ismindeki view'i dön...
+        }
+    }
+}
---
+++ MvcGiris/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcGiris.Controllers
+{
+    // Controller sınıflarımız IsimController olarak tanımlanmalıdır... Ve Controllers klasöründe olmalıdır..
+    public class HomeController : Controller
+    {
+        // Request Controllerin metoduna yapılır...
+        // Controller içerisindeki metotlara action metot deriz. Action metotlar dışarıya açık http reqeusti ile erişilebilen metotlardır...
+        public IActionResult Index()
+        {
+            // Denetleyici (Controller) yapılan requeste view dönecekse Views klasörü altında kendi ismi ile oluşturulmuş klasör içinde view arar....
+
+            // Default actionmetot isimleri ile viewname aranmaktadır. Bizde bu davranışı bozmayalım eğer view döneceksek view name ile metot name aynı olsun...
+            return View();
+
+         
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public string MesajVer()
+        {
+            return "Merhaba";
+        }
+        public int IslemYap()
+        {
+            return 2 + 2;
+        }
+        public DateTime TarihGoster()
+        {
+            return DateTime.Now;
+        }
+    }
+}
